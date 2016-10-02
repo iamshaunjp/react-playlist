2e177140856ce9f9d7b67bcaad9abcd7002bc6bc
Shaun Pelling <shaunjp@shaun-pelling.com>
2016-10-02 09:24:09 +0100

    lesson 4 code added

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,13 @@
-alert('it works');
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+//Create a component
+var TodoComponent = React.createClass({
+    render: function(){
+        return(
+            <h1>Hellooooo</h1>
+        );
+    }
+});
+
+ReactDOM.render(<TodoComponent />, document.getElementById('todo-wrapper'));
