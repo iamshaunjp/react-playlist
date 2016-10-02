8cca17c24ad6b1095130a6711679dec1eb457f72
Shaun Pelling <shaunjp@shaun-pelling.com>
2016-10-02 22:13:40 +0100

    lesson 5 code added

--- src/app/index.js
+++ src/app/index.js
@@ -5,9 +5,15 @@ var ReactDOM = require('react-dom');
 var TodoComponent = React.createClass({
     render: function(){
         return(
-            <h1>Hellooooo</h1>
+            <div>
+                <p><strong>Cheese name: </strong> {this.props.cheese.name}</p>
+                <p><strong>Cheese smell factor: </strong> {this.props.cheese.smellFactor}</p>
+                <p><strong>Cheese price: </strong>£{this.props.cheese.price}</p>
+            </div>
         );
     }
 });
 
-ReactDOM.render(<TodoComponent />, document.getElementById('todo-wrapper'));
+var myCheese = {name: 'Camembert', smellFactor: 'Extreme pong', price: 3.50 };
+
+ReactDOM.render(<TodoComponent cheese={myCheese} />, document.getElementById('todo-wrapper'));
