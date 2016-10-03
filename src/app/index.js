f886c31fb2b27157e8236ccd012be60a5ed86f71
Shaun Pelling <shaunjp@shaun-pelling.com>
2016-10-03 16:59:14 +0100

    lesson 9 code

--- src/app/index.js
+++ src/app/index.js
@@ -11,15 +11,25 @@ var TodoComponent = React.createClass({
     render: function(){
         var todos = this.state.todos;
         todos = todos.map(function(item, index){
-            return(<TodoItem key={index} item={item} />);
-        });
+            return(<TodoItem key={index} item={item} onDelete={this.onDelete} />);
+        }.bind(this));
         return(
             <div id="todo-list">
                 <p>The busiest people have the most leisure...</p>
                 <ul>{todos}</ul>
             </div>
         );
-    } //render
+    }, //render
+
+    //Custom functions
+    onDelete: function(item){
+        var updatedTodos = this.state.todos.filter(function(val, index){
+            return item !== val;
+        });
+        this.setState({
+          todos: updatedTodos
+        });
+    }
 });
 
 //Create TodoItem component
@@ -29,9 +39,15 @@ var TodoItem = React.createClass({
             <li>
                 <div className="todo-item">
                     <span className="item-name">{this.props.item}</span>
+                    <span className="item-remove" onClick={this.handleDelete}> x </span>
                 </div>
             </li>
         );
+    },
+
+    //Custom functions
+    handleDelete: function(){
+        this.props.onDelete(this.props.item);
     }
 });
 
