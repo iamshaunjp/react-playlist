e43c53f561c54a3d1e3ce03061e057250e16e263
Kenneth Ashley Edwards <kenneth.ashley@gmail.com>
2017-04-10 19:54:10 -0700

    initial webpack config working with bundle

---
+++ src/app/index.js
@@ -0,0 +1,3 @@
+'use strict';
+
+alert('it works');
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var path = require('path');
+
+module.exports = {
+
+  entry: path.resolve(__dirname, 'src') + '/app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist') + '/app',
+    filename: 'bundle.js',
+    publicPath: '/app/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      }
+    ]
+  }
+};
