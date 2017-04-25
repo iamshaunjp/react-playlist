38f3e50d08c7d348d854333448f61e31adf997d4
Carolina Vasquez-Ceja <cejac10@gmail.com>
2017-04-24 21:39:57 -0700

    simple component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,12 @@
-alert('Hi')
+let React = require('react');
+let reactDom = require('react-dom');
+
+let TodoComponent = React.createClass({
+  render: function(){
+    return(
+      <h1>HiHi</h1>
+    );
+  }
+});
+
+reactDom.render(<TodoComponent/>, document.getElementById('todo-wrapper'))
