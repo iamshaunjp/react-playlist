b2adf4283becfcf2e566740889db93f619208f39
Carolina Vasquez-Ceja <cejac10@gmail.com>
2017-04-24 21:52:23 -0700

    expanding on component

--- src/app/index.js
+++ src/app/index.js
@@ -1,12 +1,16 @@
-let React = require('react');
+let React = require('react'); //var needs to be capitalized
 let reactDom = require('react-dom');
 
 let TodoComponent = React.createClass({
-  render: function(){
+  render: function(){  //the only required method to return JSX or html
     return(
-      <h1>HiHi</h1>
+      <div> //all must be in a parent container. two indepednent parent containers will not render to DOM in REACT
+        <h1>HiHi</h1> //this is JSX, I guess.
+        <p>How/s it going?!</p>
+      </div>
+
     );
   }
 });
 
-reactDom.render(<TodoComponent/>, document.getElementById('todo-wrapper'))
+reactDom.render(<TodoComponent/>, document.getElementById('todo-wrapper')) //rendering it to the DOM
