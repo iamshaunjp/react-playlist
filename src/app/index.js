7b4de4cedc094b080510c56e299e49254f54220c
Carolina Vasquez-Ceja <cejac10@gmail.com>
2017-04-24 21:53:22 -0700

    removed comments

--- src/app/index.js
+++ src/app/index.js
@@ -2,10 +2,10 @@ let React = require('react'); //var needs to be capitalized
 let reactDom = require('react-dom');
 
 let TodoComponent = React.createClass({
-  render: function(){  //the only required method to return JSX or html
+  render: function(){
     return(
-      <div> //all must be in a parent container. two indepednent parent containers will not render to DOM in REACT
-        <h1>HiHi</h1> //this is JSX, I guess.
+      <div> 
+        <h1>HiHi</h1>
         <p>How/s it going?!</p>
       </div>
 
