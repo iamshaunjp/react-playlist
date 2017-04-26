85e116f3d19d4db9bd1db9ca734096494f611ab2
Jessica Vasquez-Soltero <jmichellevs@gmail.com>
2017-04-25 20:30:20 -0700

    playing around with props

--- src/app/index.js
+++ src/app/index.js
@@ -6,10 +6,16 @@ const TodoComponent = React.createClass({
   render: function(){
     return (
       // This is JSX
-      <h1>Helloooo</h1>
+      <div>
+        <p><strong>Cheese Name:</strong> {this.props.cheese.name}</p>
+        <p><strong>Cheese Smell Factor:</strong> {this.props.cheese.smellFactor}</p>
+        <p><strong>Cheese Price:</strong> {this.props.cheese.price}</p>
+      </div>
     )
   }
 })
 
+const myCheese = {name: 'Camembert', smellFactor: 'Extreme pong', price: '3.50'}
+
 // Put component into html page
-ReactDom.render(<TodoComponent />, document.getElementById('todo-wrapper'))
+ReactDom.render(<TodoComponent mssg="I like cheese" cheese={myCheese}/>, document.getElementById('todo-wrapper'))
