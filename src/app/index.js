01712261b582313138c667a35708078405cbf9b0
Jessica Soltero <jmichellevs@gmail.com>
2017-04-26 15:32:11 -0700

    modularizing code

--- src/app/index.js
+++ src/app/index.js
@@ -2,6 +2,11 @@
 
 const React = require('react')
 const ReactDom = require('react-dom')
+require('./css/index.css')
+
+// Module requires
+// we can use this require statement/split up our code is because we are using webpack
+const TodoItem = require('./todo-item')
 
 // Create component
 const TodoComponent = React.createClass({
@@ -46,25 +51,6 @@ const TodoComponent = React.createClass({
     this.setState({
       todos: updatedTodos
     })
-  }
-})
-
-// Create TodoItem component
-const TodoItem = React.createClass({
-  render: function() {
-    return (
-      <li>
-        <div className="todo-item">
-          <span className="item-name">{this.props.item}</span>
-          <span className="item-delete" onClick={this.handleDelete}> x </span>
-        </div>
-      </li>
-    )
-  },
-
-  // Custom functions
-  handleDelete: function() {
-    this.props.onDelete(this.props.item)
   }
 })
 
