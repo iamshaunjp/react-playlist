0c434f83cd0e81c11e1c9749b73cd6d7ccaab468
Jessica Vasquez-Soltero <jmichellevs@gmail.com>
2017-04-25 20:13:11 -0700

    create first react component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,15 @@
-alert('It woooorks!')
+const React = require('react')
+const ReactDom = require('react-dom')
+
+// Create component
+const TodoComponent = React.createClass({
+  render: function(){
+    return (
+      // This is JSX
+      <h1>Helloooo</h1>
+    )
+  }
+})
+
+// Put component into html page
+ReactDom.render(<TodoComponent />, document.getElementById('todo-wrapper'))
