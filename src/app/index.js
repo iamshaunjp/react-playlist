02b37a8ea969d42d6eedcdffaa2f200b34ea461a
Jessica Soltero <jmichellevs@gmail.com>
2017-04-26 13:04:44 -0700

    changing state

--- src/app/index.js
+++ src/app/index.js
@@ -5,14 +5,22 @@ const ReactDom = require('react-dom')
 const TodoComponent = React.createClass({
   getInitialState: function() {
     return {
-      todos: ['wash up', 'eat lunch', 'take nap']
+      todos: ['wash up', 'eat lunch', 'take nap'],
+      age: 27
     }
   },
   render: function() {
+    const ager = setTimeout(function() {
+      this.setState({
+        age: 35
+      })
+    }.bind(this), 5000)
+
     return (
       // This is JSX
       <div id="todo-list">
         <p>The busiest people have the most leisure...</p>
+        <p>{this.state.age}</p>
         <ul>
           <li>{this.state.todos[0]}</li>
           <li>{this.state.todos[1]}</li>
